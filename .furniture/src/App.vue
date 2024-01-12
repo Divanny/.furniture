a2d187b6fe13b5a596aab2938574b9cab2b2fc31
Divanny <divannyjpm@gmail.com>
2024-01-12 16:00:32 -0400

    Working on shopping cart sidebar

--- .furniture/src/App.vue
+++ .furniture/src/App.vue
@@ -1,4 +1,14 @@
 <template>
+  <div  class="bg-bluegray-900 text-gray-100 p-2 flex justify-content-between lg:justify-content-center align-items-center flex-wrap">
+      <div class="font-bold mr-8">🔥 Hot Deals!</div>
+      <div class="align-items-center hidden lg:flex">
+          <span class="line-height-3">Libero voluptatum atque exercitationem praesentium provident odit.</span>
+      </div>
+
+      <a v-ripple class="flex align-items-center no-underline justify-content-center border-circle text-gray-50 hover:bg-bluegray-700 cursor-pointer transition-colors transition-duration-150 p-ripple" style="width:2rem; height: 2rem">
+          <i class="pi pi-times"></i>
+      </a>
+  </div>
   <div class="mx-0 lg:mx-8">
     <div class="mx-0 xl:mx-8">
       <NavbarComponent/>
@@ -6,13 +16,21 @@
       <ScrollTop />
     </div>
   </div>
+  <FooterComponent/>
 </template>
 <script setup>
 import NavbarComponent from "./layout/NavbarComponent.vue";
+import FooterComponent from "./layout/FooterComponent.vue";
+
 </script>
 
 <style>
+html {
+  min-height: 100%;
+  position: relative;
+}
 body {
   margin: 0;
+  margin-bottom: 56px;
 }
 </style>
--- .furniture/src/layout/NavbarComponent.vue
+++ .furniture/src/layout/NavbarComponent.vue
@@ -1,12 +1,71 @@
 <template>
-  <div class="navbar-container d-flex justify-content-between p-3">
+  <div class="navbar-container flex justify-content-between p-3 mb-2">
+    <!-- LOGO -->
     <div class="logo" />
-    <div class="d-flex">
-      <Button size="large" icon="pi pi-truck" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded aria-label="Bookmark" />
-      <Button size="large" icon="pi pi-user" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded aria-label="Bookmark" />
-      <Button size="large" icon="pi pi-heart" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded aria-label="Bookmark" />
-      <Button size="large" icon="pi pi-shopping-bag" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded aria-label="Bookmark" />
+    <!-- SEARCH BAR -->
+    <div class="search-bar w-6 flex align-items-center">
+      <AutoComplete v-model="selectedCountry" style="" size="small" optionLabel="name" class="w-full" :suggestions="filteredCountries" @complete="search" />
     </div>
+    <!-- ITEMS -->
+    <div class="d-flex">
+      <Button size="large" icon="pi pi-truck" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded
+        aria-label="Bookmark" />
+      <Button size="large" icon="pi pi-user" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded
+        aria-label="Bookmark" />
+      <Button size="large" icon="pi pi-heart" severity="secondary" class="text-black-alpha-90 mx-1 rounded-5" text rounded
+        aria-label="Bookmark" />
+      <Button size="large" icon="pi pi-shopping-bag" @click="cartSidebar = true" severity="secondary"
+        class="text-black-alpha-90 mx-1 rounded-5" text rounded aria-label="Bookmark" />
+    </div>
+    <Sidebar v-model:visible="cartSidebar" position="right">
+      <template #header>
+        <span class="inline-flex align-items-center gap-2">
+          <span class="text-black-alpha-90 font-bold text-xl pi pi-shopping-bag"></span>
+          <span class="text-black-alpha-90 font-bold text-xl">Mi Carrito</span>
+        </span>
+      </template>
+      <div class="flex flex-column h-full">
+        <div class="overflow-y-auto">
+          <div
+            class="border-1 mb-3 border-round-sm p-2 border-200 shadow-1 hover:shadow-none hover:surface-100 transition-duration-150 transition-colors p-ripple cursor-pointer flex flex-wrap gap-2 justify-content-start"
+            rounded>
+            <div :style="{ backgroundImage: `url(${('/src/assets/empty-img.png')})` }" class="cart-product" />
+            <div class="cart-product-summary" style="max-width:60%; height: 90px">
+              <div class="text-sm	mt-1 font-semibold white-space-nowrap overflow-hidden text-overflow-ellipsis">
+                Producto Nombre asdsadsa dsad sasadsadsadsa
+              </div>
+              <div class="text-md	mt-2 font-bold white-space-nowrap overflow-hidden text-overflow-ellipsis">
+                $80.2DOP
+              </div>
+              <div class="flex text-xs cart-quantity justify-content-between" style="margin-top: 15px">
+                <InputNumber v-model="value" size="small" inputStyle="font-size: 0.75rem; padding: 0px;" :min="1"
+                  :max="100" showButtons buttonLayout="horizontal" decrementButtonClassName="cart-quantity"
+                  incrementButtonClassName="cart-quantity">
+                  <template #incrementbuttonicon>
+                    <span class="pi pi-plus" />
+                  </template>
+                  <template #decrementbuttonicon>
+                    <span class="pi pi-minus" />
+                  </template>
+                </InputNumber>
+                <Button icon="pi pi-trash" size="small" style="padding: 0px 12px" severity="danger" outlined />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mt-auto">
+          <hr class="mb-3 mx-3 border-top-1 border-none surface-border" />
+          <div class="w-full px-3">
+            <div class="flex justify-content-between mb-2">
+              <div class="text-base text-black-alpha-90 font-bold">Total:</div>
+              <div class="text-lg text-black-alpha-90 font-bold">$4965DOP</div>
+            </div>
+            <Button v-ripple label="Proceder al Pago" class="w-full mt-2 hover:teal-100 transition-duration-150 transition-colors p-ripple" />
+          </div>
+        </div>
+      </div>
+
+    </Sidebar>
   </div>
 </template>
 
@@ -14,6 +73,7 @@
 .navbar-container {
   display: flex;
 }
+
 .logo {
   height: 3rem;
   width: 90px;
@@ -22,9 +82,57 @@
   background-size: contain;
   background-image: url("../assets/logo-black-sm.png");
 }
+
+.cart-product {
+  height: 90px;
+  width: 90px;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
+.cart-quantity {
+  margin-top: auto;
+  padding: 0px !important;
+}
+
+.cart-quantity .pi {
+  font-size: x-small !important;
+}
+
+.cart-quantity .p-button {
+  width: 1rem;
+  padding: 0px;
+}
+
+.cart-quantity .p-inputtext {
+  width: 1.4rem;
+  height: 1.5rem;
+  text-align: center;
+}
+
+.search-bar .p-inputtext {
+ width: 100% !important;
+ padding: 0.5rem 0.5rem;
+ font-size: small;
+ background-color: #f5f5f5;
+}
 </style>
 
 <script>
 export default {
+  components: {},
+  data() {
+    return {
+      cartSidebar: false
+    }
+  },
+  watch: {
+
+  },
+  mounted() {
+
+  },
+  methods: {}
 };
 </script>
